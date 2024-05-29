31c20f18ceaeb2e8d25faa9fc878d5ab6fb267dd
Drag0n0wl <T20114@hotmail.com>
2024-05-30 00:07:38 +0900

    21. Write a C++ Node - With OOP

--- my_cpp_pkg/src/my_first_node.cpp
+++ my_cpp_pkg/src/my_first_node.cpp
@@ -1,11 +1,36 @@
 #include "rclcpp/rclcpp.hpp"
 
+class MyNode : public rclcpp::Node
+{
+public:
+    // write constructor, initialize the node and counter
+    MyNode() : Node("cpp_test"), counter_(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello Cpp Node");
+
+        // initilize the timer_
+        timer_ = this->create_wall_timer(std::chrono::seconds(1),
+                                         std::bind(&MyNode::timerCallback, this));
+    }
+
+private:
+    void timerCallback()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+    }
+
+    // declare the timer
+    rclcpp::TimerBase::SharedPtr timer_;
+    // declare the counter
+    int counter_;
+};
+
 int main(int argc, char **argv)
 {
     // Initialize ROS2 communication
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("cpp_test");
-    RCLCPP_INFO(node->get_logger(), "Hello Cpp Node");
+    auto node = std::make_shared<MyNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
